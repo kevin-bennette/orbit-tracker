e1ca3e9cb8685e96a216def80153df0e26bbf07e
Kevin Bennette <kevinbennete006@gmail.com>
2025-09-12 19:49:09 +0530

    Base

---
+++ src/main/java/com/gaiaorbittracker/orbittracker/OrbitTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gaiaorbittracker.orbittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbitTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaiaorbittracker/orbittracker/OrbitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaiaorbittracker.orbittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
